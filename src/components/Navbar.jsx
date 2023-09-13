9e6dae44f207c6af3c95a3ac24f652e3d31bc69c
Tibor <tibor736@hotmail.com>
2023-09-13 19:11:53 +0300

    Navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 const Navbar = () => {
 	return (
@@ -21,14 +22,16 @@ const Navbar = () => {
 						</label>
 						<ul
 							tabIndex={0}
-							className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200'
+							className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52'
 						>
-							nav links
+							<NavLinks />
 						</ul>
 					</div>
 				</div>
 				<div className='navbar-center hidden lg:flex'>
-					<ul className='menu menu-horizontal'>nav links</ul>
+					<ul className='menu menu-horizontal'>
+						<NavLinks />
+					</ul>
 				</div>
 				<div className='navbar-end'>
 					{/* Theme setup */}
@@ -37,11 +40,13 @@ const Navbar = () => {
 						to='/cart'
 						className='btn btn-ghost btn-circle btn-md ml-4 '
 					>
-            <div className="indicator">
-              <BsCart3 className='h-6 w-6'/>
-              <span className='badge badge-sm badge-primary indicator-item'>6</span>
-            </div>
-          </NavLink>
+						<div className='indicator'>
+							<BsCart3 className='h-6 w-6' />
+							<span className='badge badge-sm badge-primary indicator-item'>
+								6
+							</span>
+						</div>
+					</NavLink>
 				</div>
 			</div>
 		</nav>
