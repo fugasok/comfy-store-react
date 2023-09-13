449ed5f2b38ed092d7bd268a3f07c53ee8ff62ce
Tibor <tibor736@hotmail.com>
2023-09-14 01:09:32 +0300

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,20 @@
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className="flex flex-wrap gap-w sm:gap-x-6 items-center justify-center">
+      <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">We love</h1>
+      <div className="stats bg-primary shadow">
+        <div className="stat">
+          <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+            comfy
+          </div>
+        </div>
+      </div>
+    </div>
+    <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi aliquam porro, dolorum blanditiis sapiente illo quas libero, minus autem iste eaque in officia consequatur voluptatum non omnis, sunt maiores quos.
+    </p>
+    </>
   )
 }
 export default About
