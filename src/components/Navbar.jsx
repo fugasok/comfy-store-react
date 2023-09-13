7db94fd21dca65d3f050a55cb825b4e004c5f26c
Tibor <tibor736@hotmail.com>
2023-09-14 01:02:33 +0300

    light-dark theme toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,30 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import { useEffect, useState } from 'react';
+
+const themes = {
+	winter: 'winter',
+	forest: 'forest',
+};
+
+const getThemeFromLocalStorage = () => {
+	return localStorage.getItem('theme') || themes.winter;
+};
 
 const Navbar = () => {
+	const [theme, setTheme] = useState(getThemeFromLocalStorage());
+	const handleTheme = () => {
+		const { winter, forest } = themes;
+		const newTheme = theme === winter ? forest : winter;
+		setTheme(newTheme);
+	};
+
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', theme);
+		localStorage.setItem('theme', theme);
+	});
+
 	return (
 		<nav className='bg-base-200'>
 			<div className='navbar align-element'>
@@ -35,6 +57,11 @@ const Navbar = () => {
 				</div>
 				<div className='navbar-end'>
 					{/* Theme setup */}
+					<label className='swap swap-rotate'>
+						<input type='checkbox' onChange={handleTheme} />
+						<BsSunFill className='swap-on h-4 w-4' />
+						<BsMoonFill className='swap-off h-4 w-4' />
+					</label>
 					{/* Cart */}
 					<NavLink
 						to='/cart'
