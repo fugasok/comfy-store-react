55f601d60423692c15e1cd55bef575af74c36512
Tibor <tibor736@hotmail.com>
2023-09-15 20:05:44 +0300

    Added reducers: remove, edit, clear

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -34,9 +34,35 @@ const cartSlice = createSlice({
 			cartSlice.caseReducers.calculateTotals(state);
 			toast.success('Item added to cart');
 		},
-		clearCart: (state) => {},
-		removeItem: (state, action) => {},
-		editItem: (state, action) => {},
+		clearCart: (state) => {
+			localStorage.setItem('cart', JSON.stringify(defaultState));
+			return defaultState;
+		},
+		removeItem: (state, action) => {
+			const { cartID } = action.payload;
+			const product = state.cartItems.find((i) => i.cartID === cartID);
+			state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID);
+
+			state.numItemsInCart -= product.amount;
+			state.cartTotal -= product.price * product.amount;
+			// Call this reducer inside another reducer
+			cartSlice.caseReducers.calculateTotals(state);
+			toast.success('Item removed from cart');
+		},
+		editItem: (state, action) => {
+			const { cartID, amount } = action.payload;
+			const item = state.cartItems.find((i) => i.cartID === cartID);
+
+			// The logic here is to update the total number of items in the cart (state.numItemsInCart) by adjusting it based on the difference between the provided amount and the existing quantity of that item (item.amount). If amount is greater than item.amount, it means that items are being added to the cart. If amount is less than item.amount, it means that items are being removed from the cart. If they are equal, it implies no change to the quantity of that item in the cart.
+
+			// The result of the subtraction (amount - item.amount) is then added to the current state.numItemsInCart to reflect the new total number of items in the cart.
+			state.numItemsInCart += amount - item.amount;
+			state.cartTotal += item.price * (amount - item.amount);
+			item.amount = amount;
+			// Call this reducer inside another reducer
+			cartSlice.caseReducers.calculateTotals(state);
+			toast.success('Cart updated');
+		},
 
 		// use this reducer in other reducers
 		calculateTotals: (state) => {
