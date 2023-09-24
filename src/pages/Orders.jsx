2365f587bb1f87513e291f770c44bbdaf4d89447
Tibor <tibor736@hotmail.com>
2023-09-24 13:31:05 +0300

    Orders page loader added

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,6 +1,38 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { customFetch } from '../utils';
+import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+
+export const loader =
+	(store) =>
+	async ({ request }) => {
+		const user = store.getState().userState.user;
+
+		if (!user) {
+			toast.warning('You must be logged in to view orders');
+			return redirect('/login');
+		}
+		const params = Object.fromEntries([
+			...new URL(request.url).searchParams.entries(),
+		]);
+
+		try {
+			const response = await customFetch.get('/orders', {
+				params,
+				headers: { Authorization: `Bearer ${user.token}` },
+			});
+			return { orders: response.data.data, meta: response.data.meta };
+		} catch (error) {
+			console.log(error);
+			const errorMessage =
+				error?.response?.data?.error?.message ||
+				'there was an error placing your order';
+			toast.error(errorMessage);
+			if (error.response.status === 401 || 403) return redirect('/login');
+		}
+	};
+
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
-export default Orders
+	return <div>Orders</div>;
+};
+export default Orders;
